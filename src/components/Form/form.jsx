8f838057d808631e51359d028aeda8832fde1582
Sam Keathley <samncollette@gmail.com>
2020-09-01 14:59:42 -0700

    updated mobile style for form view

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -30,30 +30,35 @@ export default function Register() {
                 <input
                     className="rname"
                     name="rname"
+                    type="text"
                     onChange={handleChange}
                     value={contact.rname}
                     placeholder="Restaurant Name"
                 />
                 <input
                     name="address"
+                    type="text"
                     onChange={handleChange}
                     value={contact.address}
                     placeholder="Restaurant Address"
                 />
                 <input
                     name="oname"
+                    type="text"
                     onChange={handleChange}
                     value={contact.oname}
                     placeholder="Owner Name"
                 />
                 <input
                     name="email"
+                    type="email"
                     onChange={handleChange}
                     value={contact.email}
                     placeholder="Owner Email"
                 />
                 <input
                     name="phone"
+                    type="tel"
                     onChange={handleChange}
                     value={contact.phone}
                     placeholder="Phone Number"
@@ -61,6 +66,7 @@ export default function Register() {
                 <input
                     className="ftype"
                     name="ftype"
+                    type="text"
                     onChange={handleChange}
                     value={contact.ftype}
                     placeholder="Type of Cuisine"
