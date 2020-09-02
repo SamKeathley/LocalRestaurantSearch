561718a51b5a4ae1af755b41a59e270a99cb1640
Sam Keathley <samncollette@gmail.com>
2020-09-02 14:34:20 -0700

    removed service worker

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -9,115 +9,121 @@ export default function Profile() {
 
     return (
         <div className="body">
-            <Container className="foodImages">
-                <Row>
-                    <Col sm={4}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4">
                         <img src={require("../../assets/960x0.jpg")} alt="" />
-                    </Col>
-                    <Col sm={4}>
+                    </div>
+
+                    <div className="col-sm-4">
                         <img src={require("../../assets/dish3.png")} alt="" />
-                    </Col>
-                    <Col sm={4}>
+                    </div>
+
+                    <div className="col-sm-4">
                         <img src={require("../../assets/nonfeatured-traditional-mexican-foods.jpg")} alt="" />
-                    </Col>
-                </Row>
-            </Container>
-            <Container>
-                <Row>
-                    <Col sm={4}>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4">
                         <h1>Restaurant Name </h1>
-                    </Col>
+                    </div>
 
-                </Row>
-            </Container>
+                </div>
+            </div>
 
-            <Container>
-                <Row>
-                    <Col sm={1}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-1">
                         <p>open</p>
-                    </Col>
-                    <Col sm={2}>
+                    </div>
+                    <div className="col-sm-2">
                         <span className="fa fa-star checked"></span>
                         <span className="fa fa-star checked"></span>
                         <span className="fa fa-star checked"></span>
                         <span className="fa fa-star"></span>
                         <span className="fa fa-star"></span>
-                    </Col>
+                    </div>
 
-                    <Col sm={6}>
+                    <div className="col-sm-6">
 
-                    </Col>
+                    </div>
 
-                    <Col sm={3}>
+                    <div className="col-sm-3">
                         <p>Phone: (415)-974-6112</p>
-                    </Col>
+                    </div>
 
-                </Row>
-            </Container>
+                </div>
+            </div>
 
-            <Container>
-                <Row>
-                    <Col sm={2}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-2">
                         <p>Thai, Asian, Spicy</p>
-                    </Col>
+                    </div>
 
-                    <Col sm={7}>
-                    </Col>
+                    <div className="col-sm-7">
+                    </div>
 
-                    <Col sm={3}>
+                    <div className="col-sm-3">
                         <p>Website: restaurant.com</p>
-                    </Col>
+                    </div>
 
-                </Row>
-            </Container>
+                </div>
+            </div>
 
-            <Container>
-                <Row>
-                    <Col sm={2}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-2">
                         <p>11:00 AM - 5:00 PM</p>
-                    </Col>
+                    </div>
 
-                </Row>
-            </Container>
+                </div>
+            </div>
 
-            <Container>
-                <Row>
-                    <Col sm={2}>
-                        <button variant="outline-primary" className="contactButtons">Link Button</button>
-                    </Col>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-2">
+                        <p href="#" className="contactButtons">Link Button</p>
+                    </div>
 
-                    <Col sm={2}>
-                        <button variant="outline-primary" className="contactButtons">Link Button</button>
-                    </Col>
+                    <div className="col-sm-2">
+                        <p href="#" className="contactButtons">Link Button</p>
+                    </div>
 
-                    <Col sm={2}>
-                        <button variant="outline-primary" className="contactButtons">Link Button</button>
-                    </Col>
-                </Row>
-            </Container>
+                    <div className="col-sm-2">
+                        <p href="#" className="contactButtons">Link Button</p>
+                    </div>
+                </div>
+            </div>
 
-            <Container>
-                <Row>
-                    <Col sm={6}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+
                         <div id="map" className="blackMapClass"></div>
 
                         <script async defer
                             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjWfWoYtAhvc6i9C4V5SekngUKeyXOYoc
         &callback=initMap">
                         </script>
-                    </Col>
+                    </div>
 
-                    <Col sm={6}>
+                    <div className="col-sm-6">
                         <div className="scrollable">
-                            <h1>Comments </h1>
+                            <h1> Comments </h1>
                             <li>Comment 1</li>
                             <li>Comment 1</li>
                         </div>
-                    </Col>
+                    </div>
 
-                </Row>
-            </Container>
-
+                </div>
+            </div>
         </div>
     );
 }
